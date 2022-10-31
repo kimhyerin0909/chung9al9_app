906a44a0b00dfdc7de83e5597ab0e9abe1629fee
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-01 08:16:46 +0900

    Doc - 폴더 구조 변경

---
+++ screens/auth/signup/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
+
+export const SignUp = ({ navigation }) => {
+  const [userId, setUserId] = useState();
+  const [password, setPassword] = useState();
+
+  const signUp = () => {};
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Image
+          source={require("ch9al9-app/assets/logobgx.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.signupText}>SIGNUP</Text>
+        <View style={styles.btnBox}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("YouthSignUp")}
+          >
+            <Text style={styles.signup} onPress={signUp}>
+              청소년 사용자
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.signup} onPress={signUp}>
+              사업자
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: "70%",
+    height: "30%",
+    justifyContent: "space-between",
+  },
+  signupText: {
+    fontSize: 30,
+    fontWeight: "200",
+    marginBottom: 20,
+  },
+  logo: {
+    resizeMode: "contain",
+    marginLeft: "-20%",
+    height: 60,
+    marginBottom: 10,
+  },
+  input: {
+    width: "100%",
+    height: 60,
+    padding: 20,
+    backgroundColor: "#F5F5F5",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "#7C81FF",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "48%",
+    height: 50,
+    backgroundColor: "#7C81FF",
+    borderRadius: 10,
+  },
+  signup: { color: "white" },
+  btnBox: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+});
