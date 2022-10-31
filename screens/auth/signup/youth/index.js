f720c8224a3b9c4bd0f9b9ed420253c6368aa88e
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-01 08:17:01 +0900

    Style - 청소년 사용자 회원가입

---
+++ screens/auth/signup/youth/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
+
+export const YouthSignUp = ({ navigation }) => {
+  const [userId, setUserId] = useState();
+  const [password, setPassword] = useState();
+
+  const signUp = () => {};
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Image
+          source={require("ch9al9-app/assets/logobgx.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.signUpText}>SIGNUP</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="아이디를 입력해주세요."
+          onChangeText={(id) => setUserId(id)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="비밀번호를 입력해주세요."
+          secureTextEntry
+          onChangeText={(pw) => setPassword(pw)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="비밀번호를 다시 입력해주세요."
+          secureTextEntry
+          onChangeText={(pw) => setPassword(pw)}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.signUp} onPress={signUp}>
+            회원가입
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    width: "70%",
+    height: "45%",
+  },
+  signUpText: {
+    fontSize: 30,
+    fontWeight: "200",
+  },
+  logo: {
+    resizeMode: "contain",
+    marginLeft: "-20%",
+    height: 60,
+  },
+  input: {
+    width: "100%",
+    height: 60,
+    padding: 20,
+    backgroundColor: "#F5F5F5",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "#7C81FF",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 40,
+    backgroundColor: "#7C81FF",
+    borderRadius: 10,
+  },
+  signUp: { color: "white" },
+});
