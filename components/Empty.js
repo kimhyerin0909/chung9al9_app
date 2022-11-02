d71e3f3e4a0d4daf908644a5caae4356c09a3a2e
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-02 17:54:12 +0900

    Style - 노치 부분 background

---
+++ components/Empty.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export const Empty = () => {
+  return (
+    <View
+      style={{ width: "100%", height: "7%", backgroundColor: "white" }}
+    ></View>
+  );
+};
