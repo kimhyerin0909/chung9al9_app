b888a4b23da24e6bda5cbadd51081f5461d02c52
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-03 19:06:01 +0900

    Feat - recoil root

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { RecoilRoot } from "recoil";
 import { Empty } from "./components/Empty";
 import Stack from "./components/Stack";
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Empty />
-      <Stack />
+      <RecoilRoot>
+        <Empty />
+        <Stack />
+      </RecoilRoot>
     </NavigationContainer>
   );
 };
