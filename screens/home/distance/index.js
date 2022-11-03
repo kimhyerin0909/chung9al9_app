17187fbac9548e317c1612b2317ab73ee58dd57e
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-03 19:08:13 +0900

    Feat - 거리 범위 설정

---
+++ screens/home/distance/index.js
@@ -0,0 +1,43 @@
+import Slider from "@react-native-community/slider";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useRecoilState } from "recoil";
+import { postDistance } from "../../recoil/distance";
+
+export const Distance = () => {
+  const [distance, setDistance] = useRecoilState(postDistance);
+  return (
+    <View style={styles.layout}>
+      <Text style={styles.locationText}>
+        {distance <= 2000
+          ? "우리 동네"
+          : distance > 2000 && distance <= 3000
+          ? "옆 동네"
+          : "옆옆 동네"}
+      </Text>
+      <Slider
+        style={{ width: "80%" }}
+        maximumValue={4000}
+        minimumValue={1000}
+        value={distance}
+        step={1000}
+        onValueChange={(value) => setDistance(value)}
+        minimumTrackTintColor={"#8E92FF"}
+        tapToSeek
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  layout: {
+    width: "90%",
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  locationText: {
+    width: "20%",
+  },
+});
