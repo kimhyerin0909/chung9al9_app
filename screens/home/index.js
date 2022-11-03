5ad6b15ac32606f9ce0b902d9a23f7c4256e76a2
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-03 19:09:15 +0900

    Feat - 위치 정보 이용 확인, 위경도 가져오기, 글 가져오기
    글 가져올 때 위경도 데이터가 안 들어가서 오류 남

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,14 +1,64 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import React from "react";
-import { Button, Text, View } from "react-native";
+import React, { useEffect, useMemo } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { Layout } from "../../components/Layout";
+import client from "../../utils/client";
+import { postDistance } from "../recoil/distance";
+import { Distance } from "./distance";
+import { Button, Platform } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import { locationState } from "../recoil/location";
 
-export const HomeScreen = ({ navigation }) => {
+export const HomeScreen = () => {
+  const distance = useRecoilValue(postDistance);
+  const [location, setLocation] = useRecoilState(locationState);
+
+  const allowGetLocation = async () => {
+    try {
+      if (Platform.OS === "ios") {
+        return await Geolocation.requestAuthorization("whenInUse");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getPosts = async () => {
+    if (location !== null) {
+      const response = await client.get(
+        `getPost?lat=${location.lat}&lon=${location.lon}&distance=${distance}`
+      );
+      console.log(response);
+    }
+  };
+
+  useEffect(() => {
+    allowGetLocation().then((result) => {
+      if (result === "granted") {
+        Geolocation.getCurrentPosition(
+          (pos) => {
+            const { latitude, longitude } = pos.coords;
+            setLocation({
+              lat: latitude,
+              lon: longitude,
+            });
+          },
+          (err) => console.log(err),
+          {
+            enableHighAccuracy: true,
+            timeout: 3600,
+            maximumAge: 3600,
+          }
+        );
+        getPosts();
+      } else {
+        console.log(result);
+      }
+    });
+  });
   return (
-    <View>
-      <Button title="restart" onPress={() => AsyncStorage.removeItem("token")}>
-        다시
-      </Button>
-      <Text>HomeScreen</Text>
-    </View>
+    <Layout>
+      <Distance />
+      {/* <Button title="geo" onPress={getPosts} /> */}
+    </Layout>
   );
 };
