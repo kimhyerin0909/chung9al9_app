7d1709088c517ddd9204ab71a0d764f3bf6a5cdf
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-24 19:27:11 +0900

    Feat - mark worked dates
    캘린더에 알바한 날짜를 보여주고, 시간별로 배경색이 다름

--- screens/diary/index.js
+++ screens/diary/index.js
@@ -1,10 +1,65 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { Calendar } from "react-native-calendars";
+import { useQuery } from "react-query";
+import { useRecoilValue } from "recoil";
+import { Layout } from "../../components/Layout";
+import client from "../../utils/client";
+import { userIdState } from "../recoil/user";
 
 export const Diary = () => {
+  const userId = useRecoilValue(userIdState);
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [worked, setWorked] = useState();
+
+  const getRecords = async () => {
+    const res = await client.get(`getRecords?user_id=${userId}&month=${month}`);
+    res.data.forEach((data) => {
+      let date = data["date"];
+      let h = data["hour"];
+      setWorked((prev) => ({
+        ...prev,
+        [date]: {
+          selected: true,
+          selectedColor:
+            h > 7
+              ? "#A1A5FF"
+              : h > 5
+              ? "#C9CBFF"
+              : h > 3
+              ? "#E1E2FF"
+              : "#EEEFFF",
+        },
+      }));
+    });
+  };
+
+  const recordQuery = useQuery(["record"], getRecords);
+
+  useEffect(() => {
+    getRecords();
+  }, [month]);
+
   return (
-    <View>
-      <Text>diary</Text>
-    </View>
+    <Layout>
+      {recordQuery.isSuccess ? (
+        <Calendar
+          theme={{
+            selectedDayBackgroundColor: "white",
+            todayDotColor: "#FFEE52",
+            arrowColor: "black",
+          }}
+          markedDates={worked}
+          monthFormat={"yyyy년 MM월"}
+          onMonthChange={(mo) => setMonth(mo["month"])}
+        />
+      ) : null}
+    </Layout>
   );
 };
+const styles = StyleSheet.create({
+  calendar: {
+    width: "100%",
+    height: "100%",
+  },
+});
