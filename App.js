6e6bc2a88850024056e05b103e0d28b387d49eca
kimhyerin0909 <khyerin0909@gmail.com>
2022-11-24 19:21:33 +0900

    Feat - Query Client Provider

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import { Empty } from "./components/Empty";
 import Stack from "./components/Stack";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <NavigationContainer>
       <RecoilRoot>
-        <Empty />
-        <Stack />
+        <QueryClientProvider client={queryClient}>
+          <Empty />
+          <Stack />
+        </QueryClientProvider>
       </RecoilRoot>
     </NavigationContainer>
   );
