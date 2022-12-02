113188eadfdd86a9763f5f1aba78b3a6756f13e9
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-02 10:30:13 +0900

    Style - layout
    background color 지정 가능하게끔

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { View } from "react-native";
 
-export const Layout = ({ children, height }) => {
+export const Layout = ({ children, height, bg }) => {
   return (
     <View
       style={{
         width: "100%",
         height: height ? height : "100%",
         alignItems: "center",
-        backgroundColor: "white",
+        backgroundColor: bg ? bg : "white",
       }}
     >
-      <View style={{ width: "80%", alignItems: "center" }}>{children}</View>
+      <View style={{ width: "80%" }}>{children}</View>
     </View>
   );
 };
