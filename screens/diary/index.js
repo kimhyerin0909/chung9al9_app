3930d9fdd391771e670e0d1f990060905fd4e87d
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-02 10:40:39 +0900

    Feat - calendar
    day 클릭 시 추가 팝업, 일한 시간 색으로 표시, 다이어리 디자인 커스튬

--- screens/diary/index.js
+++ screens/diary/index.js
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { View } from "react-native";
 import { Calendar } from "react-native-calendars";
 import { useQuery } from "react-query";
 import { useRecoilValue } from "recoil";
 import { Layout } from "../../components/Layout";
 import client from "../../utils/client";
 import { userIdState } from "../recoil/user";
+import { DiaryAdd } from "./add";
+import { WorkInfo } from "./workInfo";
 
 export const Diary = () => {
   const userId = useRecoilValue(userIdState);
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [worked, setWorked] = useState();
+  const day = new Date();
+  const today = `${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`;
+  const [month, setMonth] = useState(day.getMonth() + 1);
+  const [worked, setWorked] = useState({
+    [today]: { marked: true, dotColor: "#DCDCDC" },
+  });
+  const [workedTime, setWorkedTime] = useState(0);
+  const [clickedDate, setClickedDate] = useState();
+  const [isDayClicked, setIsDayClicked] = useState(false);
 
   const getRecords = async () => {
+    setWorkedTime(0);
     const res = await client.get(`getRecords?user_id=${userId}&month=${month}`);
     res.data.forEach((data) => {
+      setWorkedTime((prev) => prev + Number(data["hour"]));
       let date = data["date"];
       let h = data["hour"];
       setWorked((prev) => ({
@@ -34,6 +45,14 @@ export const Diary = () => {
     });
   };
 
+  const handleRecord = (data) => {
+    setClickedDate({
+      date: data["dateString"],
+      isRecorded: data["dateString"] in worked,
+    });
+    setIsDayClicked((prev) => !prev);
+  };
+
   const recordQuery = useQuery(["record"], getRecords);
 
   useEffect(() => {
@@ -42,24 +61,35 @@ export const Diary = () => {
 
   return (
     <Layout>
-      {recordQuery.isSuccess ? (
-        <Calendar
-          theme={{
-            selectedDayBackgroundColor: "white",
-            todayDotColor: "#FFEE52",
-            arrowColor: "black",
-          }}
-          markedDates={worked}
-          monthFormat={"yyyy년 MM월"}
-          onMonthChange={(mo) => setMonth(mo["month"])}
+      {isDayClicked && (
+        <DiaryAdd
+          date={clickedDate}
+          closePopUp={() => setIsDayClicked(false)}
         />
-      ) : null}
+      )}
+      <View style={{ width: "100%", marginTop: "5%" }}>
+        {recordQuery.isSuccess ? (
+          <Calendar
+            theme={{
+              monthTextColor: "#363CCE",
+              textMonthFontWeight: "500",
+              selectedDayBackgroundColor: "white",
+              selectedDayTextColor: "#4148FF",
+              todayTextColor: "#515151",
+              dayTextColor: "#515151",
+              textDayFontWeight: "400",
+              arrowColor: "#363CCE",
+              todayDotColor: "yellow",
+              textSectionTitleColor: "#7C81FF",
+            }}
+            markedDates={worked}
+            monthFormat={"yyyy년 MM월"}
+            onMonthChange={(mo) => setMonth(mo["month"])}
+            onDayPress={handleRecord}
+          />
+        ) : null}
+        {/* <WorkInfo workedTime={workedTime} /> */}
+      </View>
     </Layout>
   );
 };
-const styles = StyleSheet.create({
-  calendar: {
-    width: "100%",
-    height: "100%",
-  },
-});
