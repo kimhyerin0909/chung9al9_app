d947c30d5377628b5f7a2b84d0d6f74547258d7a
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-12 14:57:18 +0900

    Feat - working hour, salary

--- screens/mypage/index.js
+++ screens/mypage/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { useRecoilState, useRecoilValue } from "recoil";
+import client from "../../utils/client";
+import { hourState, salaryState, userIdState } from "../recoil/user";
+import { EditWork } from "./edit ";
 
 export const MyPage = () => {
+  const userId = useRecoilValue(userIdState);
+  const [salary, setSalary] = useRecoilState(salaryState);
+  const [hour, setHour] = useRecoilState(hourState);
+  const [userInfo, setUserInfo] = useState();
+  const [isChange, setIsChange] = useState(false);
+  const getUserinfo = async () => {
+    const res = await client.get(`/getUserInfo?user_id=${userId}`);
+    setUserInfo(res.data);
+    setSalary(String(res.data["salary"]));
+    setHour(String(res.data["hour"]));
+  };
+
+  useEffect(() => {
+    getUserinfo();
+  }, [isChange]);
+
   return (
     <View>
-      <Text>MyPage</Text>
+      {userInfo &&
+        (userInfo["salary"] === 0 ? (
+          <EditWork userId={userId} setIsChange={setIsChange} />
+        ) : (
+          <>
+            <Text>나의 하루 근무 시간은</Text>
+            <Text>{userInfo["hour"]}시간입니다.</Text>
+            <Text>나의 시급은</Text>
+            <Text>{userInfo["salary"]}원입니다.</Text>
+          </>
+        ))}
     </View>
   );
 };
