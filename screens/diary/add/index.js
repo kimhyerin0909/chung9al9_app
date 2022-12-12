d398626bdc13ed5e176591ff09c34d488fceed9d
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-12 14:56:44 +0900

    Feat - add or fix record

--- screens/diary/add/index.js
+++ screens/diary/add/index.js
@@ -3,10 +3,11 @@ import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 import { useRecoilValue } from "recoil";
 import { TimePicker } from "../../../components/TimePicker";
 import client from "../../../utils/client";
-import { userIdState } from "../../recoil/user";
+import { salaryState, userIdState } from "../../recoil/user";
 
-export const DiaryAdd = ({ date, closePopUp }) => {
+export const DiaryAdd = ({ date, closePopUp, setIsChange }) => {
   const userId = useRecoilValue(userIdState);
+  const salary = useRecoilValue(salaryState);
   const [record, setRecord] = useState();
   const [startTime, setStartTime] = useState(720);
   const [endTime, setEndTime] = useState(720);
@@ -14,12 +15,39 @@ export const DiaryAdd = ({ date, closePopUp }) => {
     const res = await client.get(
       `/getDateRecord?user_id=${userId}&date=${date["date"]}`
     );
-    setRecord(res.data);
+    if (res.data !== "") {
+      setRecord(res.data);
+      setStartTime(
+        Number(res.data["start_time"].substr(0, 2)) * 60 +
+          Number(res.data["start_time"].substr(3, 2))
+      );
+      setEndTime(
+        Number(res.data["end_time"].substr(0, 2)) * 60 +
+          Number(res.data["end_time"].substr(3, 2))
+      );
+    }
   };
 
-  //   const recordWork = async () => {
-
-  //   }
+  const recordWork = async () => {
+    const st = `${Math.floor(startTime / 60)}:${startTime % 60}`;
+    const et = `${Math.floor(endTime / 60)}:${endTime % 60}`;
+    if (date["isRecorded"]) {
+      await client.patch(
+        `/fixRecord?cal_id=${record["cal_id"]}&start_time=${st}&end_time=${et}`
+      );
+    } else {
+      await client.post("/record", {
+        cal_id: 0,
+        user_id: userId,
+        date: date["date"],
+        start_time: st,
+        end_time: et,
+        hour: (endTime - startTime) / 60,
+        salary: salary,
+      });
+    }
+    setIsChange((prev) => !prev);
+  };
 
   return (
     <Modal
@@ -40,7 +68,12 @@ export const DiaryAdd = ({ date, closePopUp }) => {
           <Text style={{ color: "#989898" }}>{date["date"]}</Text>
           <View style={{ marginBottom: 10 }}>
             <Text
-              style={{ marginBottom: 7, color: "#7A7A7A", fontWeight: "600" }}
+              style={{
+                marginBottom: 7,
+                marginTop: 7,
+                color: "#7A7A7A",
+                fontWeight: "600",
+              }}
             >
               근무한 시간
             </Text>
@@ -55,9 +88,15 @@ export const DiaryAdd = ({ date, closePopUp }) => {
             <Pressable style={styles.btn} onPress={closePopUp}>
               <Text style={{ color: "white", fontWeight: "700" }}>취소</Text>
             </Pressable>
-            <Pressable style={styles.btn} onPress={closePopUp}>
+            <Pressable
+              style={styles.btn}
+              onPress={() => {
+                recordWork();
+                closePopUp();
+              }}
+            >
               <Text style={{ color: "white", fontWeight: "700" }}>
-                기록하기
+                {date["isRecorded"] ? "수정하기" : "기록하기"}
               </Text>
             </Pressable>
           </View>
