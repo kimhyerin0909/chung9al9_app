5ff32dcd854c3b04fe43dfae3f8772c99bcaa434
kimhyerin0909 <khyerin0909@gmail.com>
2022-12-15 12:20:04 +0900

    Style - 수직 정렬

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 
-export const Layout = ({ children, height, bg }) => {
+export const Layout = ({ children, height, bg, just }) => {
   return (
     <View
       style={{
         width: "100%",
         height: height ? height : "100%",
         alignItems: "center",
+        justifyContent: just ? just : "",
         backgroundColor: bg ? bg : "white",
       }}
     >
